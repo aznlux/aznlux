1373bcad374b2a8924539d315e3b661968743e2a
bngrid <bngrid@outlook.com>
2025-10-27 10:36:38 +0800

    添加pnpm

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,26 @@
 'use client'
 
-// import { SWRConfig } from 'swr'
-// import { Demo } from '@/components/demo'
-// import { useBear } from '@/stores/bear'
-
-// export default function Home() {
-//   const bears = useBear(state => state.bears)
-//   return (
-//     <div>
-//       <SWRConfig
-//         value={{
-//           onLoadingSlow: () => {
-//             console.log('加载时间过长')
-//           },
-//           onSuccess: data => {
-//             console.log(data)
-//           },
-//         }}
-//       >
-//         <Demo />
-//         <div className="mt-4">{bears}</div>
-//       </SWRConfig>
-//     </div>
-//   )
-// }
+import { SWRConfig } from 'swr'
+import { Demo } from '@/components/demo'
+import { useBear } from '@/stores/bear'
 
 export default function Home() {
-  return <div>hello</div>
+  const bears = useBear(state => state.bears)
+  return (
+    <div>
+      <SWRConfig
+        value={{
+          onLoadingSlow: () => {
+            console.log('加载时间过长')
+          },
+          onSuccess: data => {
+            console.log(data)
+          },
+        }}
+      >
+        <Demo />
+        <div className="mt-4">{bears}</div>
+      </SWRConfig>
+    </div>
+  )
 }
